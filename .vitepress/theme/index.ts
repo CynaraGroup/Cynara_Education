ea6d284770e250eafc3fc501aca1c1590da8bf5d
ZhngJah <884256028@qq.com>
2025-08-19 11:51:17 +0800

    修复group-icons

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,6 @@
 import DefaultTheme from "vitepress/theme";
 import "@catppuccin/vitepress/theme/macchiato/lavender.css";
 import './style/index.css'
-import 'virtual:group-icons.css' //代码组样式
 
 export default {
     extends: DefaultTheme,
