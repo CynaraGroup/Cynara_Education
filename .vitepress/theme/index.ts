7b465dbf32703ec020141ab486ae1983c1592313
ZhngJah <884256028@qq.com>
2025-08-19 12:29:36 +0800

    删除live2s

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,46 +4,22 @@ import './style/index.css'
 import mediumZoom from 'medium-zoom';
 import { onMounted, watch, nextTick } from 'vue';
 import { useRoute } from 'vitepress';
-import { useLive2d } from 'vitepress-theme-website'
 
 export default {
     extends: DefaultTheme,
 
     setup() {
-    const route = useRoute();
-    const initZoom = () => {
-      // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
-      mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
-    };
-    onMounted(() => {
-      initZoom();
-    });
-    watch(
-      () => route.path,
-      () => nextTick(() => initZoom())
-    );
-      
-    //看板娘
-    useLive2d({
-      enable: true,
-      model: {
-        url: 'https://raw.githubusercontent.com/iCharlesZ/vscode-live2d-models/master/model-library/hibiki/hibiki.model.json'
-      },
-      display: {
-        position: 'right',
-        width: '135px',
-        height: '300px',
-        xOffset: '35px',
-        yOffset: '5px'
-      },
-      mobile: {
-        show: true
-      },
-      react: {
-        opacity: 0.8
-      }
-    })
-  },
-
-
+        const route = useRoute();
+        const initZoom = () => {
+        // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
+        mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
+        };
+        onMounted(() => {
+        initZoom();
+        });
+        watch(
+        () => route.path,
+        () => nextTick(() => initZoom())
+        );
+    }
 }
